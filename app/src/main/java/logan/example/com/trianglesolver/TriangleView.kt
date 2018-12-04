e8d8cd6a31ebb0ae999b306792404154467d1814
Loganarmack <loganarmack@gmail.com>
2018-12-03 20:59:14 -0500

    Program now draws a triangle

--- app/src/main/java/logan/example/com/trianglesolver/TriangleView.kt
+++ app/src/main/java/logan/example/com/trianglesolver/TriangleView.kt
@@ -28,17 +28,32 @@ class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
 
 class MyGLRenderer : GLSurfaceView.Renderer {
 
-    override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
+    private lateinit var mTriangle: Triangle
+
+    override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        mTriangle = Triangle()
     }
 
     override fun onDrawFrame(unused: GL10) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        mTriangle.draw()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
+    }
+}
+
+fun loadShader(type: Int, shaderCode: String): Int {
+    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+    return GLES20.glCreateShader(type).also { shader ->
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode)
+        GLES20.glCompileShader(shader)
     }
 }
